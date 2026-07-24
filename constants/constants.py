1ae0ba1925d20dcab9c0045bebc519c5a8195e5c
den <di272004@gmail.com>
2026-07-24 14:58:38 +0300

    do refactor security

--- constants/constants.py
+++ constants/constants.py
@@ -2,3 +2,7 @@
 BASE_IMAGE_URL = "https://rms.kufar.by/v1/gallery/"
 BASE_URL = "https://api.kufar.by/search-api/v2/"
 PLACEHOLDER_IMAGE_URL = "https://cardiovitebsk.by/wp-content/uploads/2022/12/01.png"
+
+# telegram
+BOT_TOKEN = "token"
+CHAT_ID = 111
